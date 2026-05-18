66cebf090a0d9bd2cd04f6e1726cca7cd208156b
Danila Golubkov <danilgolubkov172@gmail.com>
2026-05-17 22:20:17 -0400

    fix

--- 2211A/solution.cpp
+++ 2211A/solution.cpp
@@ -9,6 +9,7 @@ int main()
     {
         int length;
         std::vector<int> vec;
+        std::vector<int> answer;
         std::cin >> length;
 
         for (int j = 0; j < length; j++)
@@ -16,6 +17,31 @@ int main()
             int num;
             std::cin >> num;
             vec.push_back(num);
-                }
+        }
+
+        std::vector<int> subvec;
+
+        for (int k = 0; k < vec.size(); k++)
+        {
+            int count{};
+            if (k == 0)
+            {
+                subvec.push_back(vec[0]);
+                subvec.push_back(vec[1]);
+                subvec.push_back(vec[2]);
+            }
+            else if ((k - 1) == vec.size())
+            {
+                subvec.push_back(vec[k]);
+                subvec.push_back(vec[k - 1]);
+                subvec.push_back(vec[k - 2]);
+            }
+            else
+            {
+                subvec.push_back(vec[k - 1]);
+                subvec.push_back(vec[k]);
+                subvec.push_back(vec[k + 1]);
+            }
+        }
     }
 }
