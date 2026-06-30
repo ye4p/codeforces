5b8aad5c0e8b069ed5cbb17b2676460a46c00cd4
Danila Golubkov <danilgolubkov172@gmail.com>
2026-06-30 12:58:53 -0400

    fix

--- 2195C/solution.cpp
+++ 2195C/solution.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
@@ -8,5 +9,37 @@ int main()
     {
         int num;
         std::cin >> num;
+        std::vector<int> vec;
+        for (int i = 0; i < num; i++)
+        {
+            int n;
+            std::cin >> n;
+            vec.push_back(n);
         }
+
+        // need to determine whether something is dice roll sequence or no
+        // Therefore, need to only check neighbours of the sequence to see if they are adjacent.
+        // Check if next number is equals to current number or if next number equals to 7 - x of current number
+
+        int cnt = 0;
+        int sqn = 0;
+        for (int i = 0; i < (vec.size() - 1); i++)
+        {
+            if (vec[i] == vec[i + 1] || (7 - vec[i]) == vec[i + 1])
+            {
+                cnt++;
+                sqn++;
+            }
+            else
+            {
+                sqn = 0;
+            }
+            if (sqn == 2)
+            {
+                cnt--;
+                sqn = 0;
+            }
+        }
+        std::cout << cnt << "\n";
+    }
 }
