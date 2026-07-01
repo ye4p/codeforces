0251d7dedb709ecaa0867f66618f37fcc5a254e2
Danila Golubkov <danilgolubkov172@gmail.com>
2026-07-01 13:00:13 -0400

    fix

--- 2234C/solution.cpp
+++ 2234C/solution.cpp
@@ -23,10 +23,30 @@ int main()
         {
             int tot{};
             int r = i;
+            int maximum = hs[i];
             while (++r < hs.size())
             {
-                tot += hs[r - 1];
+                tot += std::max(maximum, hs[r - 1]);
+                maximum = std::max(maximum, hs[r]);
             }
+            r = 0;
+            while (++r <= i)
+            {
+                int l = r - 1;
+                if (r == 1)
+                {
+                    l = hs.size() - 1;
+                }
+                tot += hs[l];
+            }
+            res.push_back(tot);
         }
+
+        // output
+        for (int i : res)
+        {
+            std::cout << i << " ";
+        }
+        std::cout << "\n";
     }
 }
